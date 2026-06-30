e889ff2047f1e88d1f2da52a779ab7ffededf16b
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-06-30 18:01:25 +0100

    Set up the home page hero section

---
+++ src/components/home/HeroSection.jsx
@@ -0,0 +1,7 @@
+export default function HeroSection() {
+    return (
+        <div className="bg-[url(/hero.png)] bg-contain bg-no-repeat h-screen">
+            <h1>Hello World!</h1>
+        </div>
+    );
+}
