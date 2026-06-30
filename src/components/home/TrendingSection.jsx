94a56af15c1a74eac86cd7fe109da557475ee174
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-06-30 23:37:31 +0100

    Set up trending section

---
+++ src/components/home/TrendingSection.jsx
@@ -0,0 +1,15 @@
+import SectionTitle from "./SectionTitle";
+
+export default function TrendingSection() {
+    return (
+        <section>
+            <div className="container py-8">
+                <SectionTitle
+                    title="Trending Now"
+                    description="WHAT THE COMMUNITY IS WATCHING"
+                />
+                <h1>Trending section</h1>
+            </div>
+        </section>
+    );
+}
